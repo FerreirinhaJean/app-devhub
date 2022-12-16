f09713ed38c7df1f97449e79b76c6b55dfa16258
Jean Ferreira <jeangabrielferreira1@gmail.com>
2022-12-16 08:37:01 -0300

    Day 3 of Code

--- app/src/main/java/br/com/jean/devhub/MainActivity.kt
+++ app/src/main/java/br/com/jean/devhub/MainActivity.kt
@@ -3,35 +3,28 @@ package br.com.jean.devhub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.com.jean.devhub.ui.theme.DevHubTheme
-import br.com.jean.devhub.ui.theme.Shapes
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,9 +53,15 @@ fun InfoPerfil() {
                 .fillMaxWidth()
                 .height(150.dp)
         )
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data("https://avatars.githubusercontent.com/u/59851490?v=4")
+                .crossfade(true)
+                .build(),
             contentDescription = "Imagem de perfil",
+            placeholder = painterResource(id = R.drawable.ic_launcher_background),
+            fallback = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentScale = ContentScale.Crop,
             modifier = Modifier
                 .size(150.dp)
                 .offset(y = (-75).dp)
@@ -71,7 +70,8 @@ fun InfoPerfil() {
         )
         Column(
             modifier = Modifier
-                .align(CenterHorizontally),
+                .align(CenterHorizontally)
+                .offset(y = (-40).dp),
             verticalArrangement = Arrangement.Center
         ) {
             Text(
